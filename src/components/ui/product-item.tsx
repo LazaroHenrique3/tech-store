6f57a971fbce803ab7b7c1090b94f04dbf3a1590
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-10-18 15:26:55 -0300

    feat: Add componentes de ProductList e ProductItem

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,35 +1,60 @@
-import { Product } from "@prisma/client";
-
-import Image from "next/image"
+import Image from "next/image";
+import { ProductsWithTotalPrice } from "@/helpers/product";
+import { Badge } from "./badge";
+import { ArrowDown } from "lucide-react";
 
 interface ProductItemProps {
-    product: Product
+  product: ProductsWithTotalPrice;
 }
 
-const ProductItem = ({product}: ProductItemProps) => {
-    return ( 
-        <div className="flex flex-col max-w-[156px] gap-4">
-            <div className="flex h-[170px] w-[156px] items-center justify-center bg-accent rounded-lg">
-                <Image 
-                    src={product.imageUrls[0]} 
-                    height={0}
-                    width={0}
-                    sizes="100vw"
-                    className="h-auto max-h-[70%] w-auto max-w-[80%]"
-                    style={{
-                        objectFit:  "contain"
-                    }}
-                    alt={product.name}
-                />
-            </div>
+const ProductItem = ({ product }: ProductItemProps) => {
+  return (
+    <div className="flex max-w-[156px] flex-col gap-4">
+      <div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+        <Image
+          src={product.imageUrls[0]}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          style={{
+            objectFit: "contain",
+          }}
+          alt={product.name}
+        />
 
-            <div>
-                <p className="w-full text-sm overflow-hidden whitespace-nowrap text-ellipsis">
-                    {product.name}
-                </p>
-            </div>
+        {product.discountPercentage > 0 && (
+          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+            <ArrowDown size={14}/> {product.discountPercentage}%
+          </Badge>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+          {product.name}
+        </p>
+
+        <div className="flex items-center gap-2">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="font-semibold">
+                R$ {product.totalPrice.toFixed(2)}
+              </p>
+
+              <p className="text-xs line-through opacity-75">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            </>
+          ) : (
+            <p className="text-sm font-semibold">
+              R$ {product.basePrice.toFixed(2)}
+            </p>
+          )}
         </div>
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default ProductItem;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,14 +3,12 @@ import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
 
-import { Environment } from "../../environment";
-
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient),
   providers: [
     GoogleProvider({
-      clientId: Environment.GOOGLE_CLIENT_ID,
-      clientSecret: Environment.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID.replace(/^"|"$/g, ''),
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET.replace(/^"|"$/g, ''),
     }),
   ],
 };

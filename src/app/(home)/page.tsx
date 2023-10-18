b4ebf417a4cd8a5fb24cf64f03f8bd388cde8a58
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-10-18 16:42:03 -0300

    feat: Adicionando a sessão de teclados

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,47 +1,48 @@
-import Image from "next/image"
+import Image from "next/image";
 
-import Categories from "./components/categories"
-import ProductList from "./components/product-list"
+import Categories from "./components/categories";
+import ProductList from "./components/product-list";
 
-import { prismaClient } from "@/lib/prisma"
+import { prismaClient } from "@/lib/prisma";
+import SectionTitle from "@/components/ui/section-title";
+import PromoBanner from "./components/promo-banner";
 
 export default async function Home() {
   const deal = await prismaClient.product.findMany({
     where: {
       discountPercentage: {
-        gt: 0
-      }
-    }
-  })
+        gt: 0,
+      },
+    },
+  });
+
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
 
   return (
-   <div>
-    <Image 
-      src="/banner-home-01.png"
-      height={0}
-      width={0}
-      className="h-auto w-full px-5"
-      sizes="100vw"
-      alt="Até 55% de desconto esse mês"
-    />
+    <div>
+      <PromoBanner src="/banner-home-01.png" alt="Até 55% de desconto esse mês"/>
 
-    <div className="mt-8 px-5">
-      <Categories />
+      <div className="mt-8 px-5">
+        <Categories />
+      </div>
+
+      <div className="mt-8">
+        <SectionTitle>Ofertas</SectionTitle>
+        <ProductList products={deal} />
+      </div>
+
+      <PromoBanner src="/banner-home-02.png" alt="Até 55% de desconto em mouses"/>
+
+      <div className="mt-8">
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
     </div>
-
-    <div className="mt-8">
-      <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
-      <ProductList products={deal}/>
-    </div>
-
-    <Image 
-      src="/banner-home-02.png"
-      height={0}
-      width={0}
-      className="h-auto w-full px-5"
-      sizes="100vw"
-      alt="Até 55% de desconto em mouses"
-    />
-   </div>
-  )
+  );
 }

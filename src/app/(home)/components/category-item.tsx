5827e58030ec6c37a656e82f243a8d8799c93e16
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-10-19 10:39:47 -0300

    feat: Adicionando Link aos Category item

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { Badge } from "@/components/ui/badge";
 import { CATEGORY_ICON } from "@/constants/category-icon";
 import { Category } from "@prisma/client";
@@ -10,6 +12,7 @@ const CategoryItem = ({category}: CategoryItemProps) => {
    
 
     return ( 
+      <Link href={`/category/${category.slug}`}>
         <Badge 
             variant='outline' 
             className="flex items-center justify-center gap-2 rounded-lg py-3"
@@ -19,6 +22,7 @@ const CategoryItem = ({category}: CategoryItemProps) => {
                 {category.name}
             </span>
         </Badge>
+      </Link>
      );
 }
  

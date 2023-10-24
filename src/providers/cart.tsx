42d0a1cd572171b605c2b4a21c62c9f7e2844ac5
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-10-24 15:22:49 -0300

    feat: Adicionando a função de reduzir quantidade no Carrinho

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -4,64 +4,86 @@ import { ProductsWithTotalPrice } from "@/helpers/product";
 import { ReactNode, createContext, useState } from "react";
 
 export interface CartProduct extends ProductsWithTotalPrice {
-    quantity: number
+  quantity: number;
 }
 
 interface ICartContext {
-    products: CartProduct[],
-    cartTotalPrice: number,
-    cartBasePrice: number,
-    cartTotalDiscount: number,
-    addProductToCart: (product: CartProduct) => void
+  products: CartProduct[];
+  cartTotalPrice: number;
+  cartBasePrice: number;
+  cartTotalDiscount: number;
+  addProductToCart: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
-    products: [],
-    cartTotalPrice: 0,
-    cartBasePrice: 0,
-    cartTotalDiscount: 0,
-    addProductToCart: () => {}
-})
+  products: [],
+  cartTotalPrice: 0,
+  cartBasePrice: 0,
+  cartTotalDiscount: 0,
+  addProductToCart: () => {},
+  decreaseProductQuantity: () => {}
+});
 
-const CartProvider = ({children}: {children: ReactNode}) => {
-    const [products, setProducts] = useState<CartProduct[]>([])
+const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
 
-    const addProductToCart = (product: CartProduct) => {
-        const productIsAlreadyOnCart = products.some(
-            (cartProduct) => cartProduct.id === product.id
-        )
+  const addProductToCart = (product: CartProduct) => {
+    const productIsAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
 
-        if (productIsAlreadyOnCart) {
-            setProducts((prev) =>
-              prev.map((cartProduct) => {
-                if (cartProduct.id === product.id) {
-                  return {
-                    ...cartProduct,
-                    quantity: cartProduct.quantity + product.quantity,
-                  };
-                }
-      
-                return cartProduct;
-              }),
-            );
+    //Verificando se este produto já existe
+    if (productIsAlreadyOnCart) {
+      setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + product.quantity,
+            };
+          }
 
-            return
-        }
+          return cartProduct;
+        }),
+      );
 
-        setProducts((prev) => [...prev, product])
+      return;
     }
 
-    return ( 
-        <CartContext.Provider value={{
-            products,
-            addProductToCart,
-            cartTotalPrice: 0,
-            cartBasePrice: 0,
-            cartTotalDiscount: 0
-        }}>
-            { children }
-        </CartContext.Provider>
-     );
-}
- 
+    setProducts((prev) => [...prev, product]);
+  };
+
+  const decreaseProductQuantity = (productId: string) => {
+    //Se a quantidade for 1 remove o produto do carrinho, caso contrário remove -1
+    setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity - 1,
+          };
+        }
+
+        return cartProduct;
+      }).filter((cartProduct) => cartProduct.quantity > 0),
+    );
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        addProductToCart,
+        decreaseProductQuantity,
+        cartTotalPrice: 0,
+        cartBasePrice: 0,
+        cartTotalDiscount: 0,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
 export default CartProvider;

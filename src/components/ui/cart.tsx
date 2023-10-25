12c2b1521e40da3341b68eba6634c506cf62af5b
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-10-25 10:32:58 -0300

    feat: Adicionando o scroll-area e o botão de Finalizar compra

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -5,12 +5,14 @@ import { useContext } from "react";
 import CartItem from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 
 const Cart = () => {
   const { products, subTotal, total, totalDiscount } = useContext(CartContext);
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -19,45 +21,53 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
+      <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-8">
+            {products.length > 0 ? (
               products.map((product) => (
                 <CartItem
                   key={product.id}
                   product={computeProductTotalPrice(product as any) as any}
                 />
               ))
-        ) : (
-            <p className="text-center">Carrinho vazio.</p>
-        )}
+            ) : (
+              <p className="text-center">Carrinho vazio.</p>
+            )}
+          </div>
+        </ScrollArea>
       </div>
 
       <div className="flex flex-col gap-3">
-            <Separator/>
+        <Separator />
 
-            <div className="flex items-center justify-between text-xs">
-                <p>SubTotal</p>
-                <p>R$ {subTotal.toFixed(2)}</p>
-            </div>
+        <div className="flex items-center justify-between text-xs">
+          <p>SubTotal</p>
+          <p>R$ {subTotal.toFixed(2)}</p>
+        </div>
 
-            <div className="flex items-center justify-between text-xs">
-                <p>Entrega</p>
-                <p>Grátis</p>
-            </div>
+        <div className="flex items-center justify-between text-xs">
+          <p>Entrega</p>
+          <p>Grátis</p>
+        </div>
 
-            <Separator/>
+        <Separator />
 
-            <div className="flex items-center justify-between text-xs">
-                <p>Descontos</p>
-                <p>- R$ {totalDiscount.toFixed(2)}</p>
-            </div>
+        <div className="flex items-center justify-between text-xs">
+          <p>Descontos</p>
+          <p>- R$ {totalDiscount.toFixed(2)}</p>
+        </div>
 
-            <Separator/>
+        <Separator />
 
-            <div className="flex items-center justify-between text-xs font-bold">
-                <p>Total</p>
-                <p>R$ {total.toFixed(2)}</p>
-            </div>
+        <div className="flex items-center justify-between text-xs font-bold">
+          <p>Total</p>
+          <p>R$ {total.toFixed(2)}</p>
+        </div>
+
+        <Button className="mt-7 font-bold uppercase">
+          Finalizar compra
+        </Button>
       </div>
     </div>
   );

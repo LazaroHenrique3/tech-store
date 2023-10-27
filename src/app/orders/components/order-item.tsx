f8bd1c5e38e25ee11a487204bee7dbdfe94e67cb
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-10-27 11:12:52 -0300

    style: Alterando a fonto do Order

--- src/app/orders/components/order-item.tsx
+++ src/app/orders/components/order-item.tsx
@@ -43,9 +43,9 @@ const OrderItem = ({order}: OrderItemProps) => {
                 <AccordionItem value={order.id}>
                     <AccordionTrigger>
                         <div className="flex flex-col gap-1 text-left">
-                            <p>Pedido com {order.orderProducts.length} produto(s)</p>
+                            <p className="font-bold uppercase">Pedido com {order.orderProducts.length} produto(s)</p>
                             <span className="text-sm opacity-60">
-                                Feito em {format(order.createdAt, "d/MM/y 'às HH:mm")}
+                                Feito em {format(order.createdAt, "d/MM/y 'às' HH:mm")}
                             </span>
                         </div>
                     </AccordionTrigger>
